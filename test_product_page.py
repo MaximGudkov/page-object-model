d7e7cfd8a789f0f085d20dc5aa96989f481b7b8a
MaximGudkov <maksimop6@gmail.com>
2023-02-27 19:10:31 +0500

    Added parameterization to product page test

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,10 @@
+import pytest
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('n', [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9])
+def test_guest_can_add_product_to_basket(browser, n):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{n}"
     page = ProductPage(browser, link)
     page.open()
     page.adding_to_basket()
